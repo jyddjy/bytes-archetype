46c9af4757307945ca9e225e8c2aa971311d46dc
麻亮 <maliang@newhope.cn>
2020-07-21 22:59:38 +0800

    chore(*): 修改配置

--- server/src/main/java/com/bytes/bfs/archetype/MainApplication.java
+++ server/src/main/java/com/bytes/bfs/archetype/MainApplication.java
@@ -3,9 +3,11 @@ package com.bytes.bfs.archetype;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @Slf4j
 @SpringBootApplication
+@ComponentScan(value = {"com.bytes.bfs"})
 public class MainApplication {
     public static void main(String[] args) {
         SpringApplication.run(MainApplication.class, args);
